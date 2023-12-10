09a9561811c745bd660b24f189c376b1706a1b5b
34948573 <hcaulfield@mailbox.org>
2023-12-10 16:48:33 +0100

    feat: solve day 2 part 2an2

--- src/day 2/index.ts
+++ src/day 2/index.ts
@@ -22,20 +22,27 @@ class Game {
   }
 }
 
-export function calculateGameIdsSum(input: string[]): number {
+export function calculateGameIdsSumAndPowers(input: string[]): {
+  gameIdsSum: number
+  minimumCubePowers: number
+} {
   const totalCubes = new Cubes(12, 13, 14)
   const preparedData = input.map((game) => prepareData(game))
 
   //   console.dir(preparedData, { depth: null })
 
-  let sum = 0
+  let gameIdsSum = 0 // puzzle part 1
+  let minimumCubePowers = 0 // puzzle part 2
 
   preparedData.forEach((game) => {
-    if (isPossible(game.cubes, totalCubes)) sum += game.id
+    if (isPossible(game.cubes, totalCubes)) gameIdsSum += game.id
+    minimumCubePowers += calculateMinimumCubesPower(game.cubes)
   })
 
-  console.log(sum)
-  return sum
+  return {
+    gameIdsSum,
+    minimumCubePowers
+  }
 }
 
 function prepareData(game: string): Game {
@@ -95,4 +102,18 @@ function isPossible(cubes: Cubes[], totalCubes: Cubes): boolean {
   })
 
   return result
+}
+
+function calculateMinimumCubesPower(cubes: Cubes[]): number {
+  let red = 0
+  let green = 0
+  let blue = 0
+
+  cubes.forEach((revealedCubes) => {
+    if (revealedCubes.r > red) red = revealedCubes.r
+    if (revealedCubes.g > green) green = revealedCubes.g
+    if (revealedCubes.b > blue) blue = revealedCubes.b
+  })
+
+  return red * green * blue
 }
--- tests/day2/day2.test.ts
+++ tests/day2/day2.test.ts
@@ -1,10 +1,26 @@
-import { calculateGameIdsSum } from '../../src/day 2'
+import { calculateGameIdsSumAndPowers } from '../../src/day 2'
 import { puzzleData, sampleData } from './testData'
 
-it('sample data test', () => {
-  expect(calculateGameIdsSum(sampleData)).toEqual(8)
+describe('part 1', () => {
+  it('sample data test - part 1', () => {
+    const { gameIdsSum } = calculateGameIdsSumAndPowers(sampleData)
+    expect(gameIdsSum).toEqual(8)
+  })
+
+  it('puzzle data test - part 1', () => {
+    const { gameIdsSum } = calculateGameIdsSumAndPowers(puzzleData)
+    expect(gameIdsSum).toEqual(2600)
+  })
 })
 
-it('puzzle data test', () => {
-  expect(calculateGameIdsSum(puzzleData)).toEqual(2600)
+describe('part 2', () => {
+  it('sample data - part 2', () => {
+    const { minimumCubePowers } = calculateGameIdsSumAndPowers(sampleData)
+    expect(minimumCubePowers).toEqual(2286)
+  })
+
+  it('puzzle data - part 2', () => {
+    const { minimumCubePowers } = calculateGameIdsSumAndPowers(puzzleData)
+    expect(minimumCubePowers).toEqual(86036)
+  })
 })
