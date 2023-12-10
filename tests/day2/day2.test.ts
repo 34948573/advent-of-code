a5f1081e412b693542712333ba56c2d0dab1df7f
34948573 <hcaulfield@mailbox.org>
2023-12-10 16:53:19 +0100

    test: change test decriptions

--- tests/day2/day2.test.ts
+++ tests/day2/day2.test.ts
@@ -2,24 +2,24 @@ import { calculateGameIdsSumAndPowers } from '../../src/day 2'
 import { puzzleData, sampleData } from './testData'
 
 describe('part 1', () => {
-  it('sample data test - part 1', () => {
+  it('sample data', () => {
     const { gameIdsSum } = calculateGameIdsSumAndPowers(sampleData)
     expect(gameIdsSum).toEqual(8)
   })
 
-  it('puzzle data test - part 1', () => {
+  it('puzzle data', () => {
     const { gameIdsSum } = calculateGameIdsSumAndPowers(puzzleData)
     expect(gameIdsSum).toEqual(2600)
   })
 })
 
 describe('part 2', () => {
-  it('sample data - part 2', () => {
+  it('sample data', () => {
     const { minimumCubePowers } = calculateGameIdsSumAndPowers(sampleData)
     expect(minimumCubePowers).toEqual(2286)
   })
 
-  it('puzzle data - part 2', () => {
+  it('puzzle data', () => {
     const { minimumCubePowers } = calculateGameIdsSumAndPowers(puzzleData)
     expect(minimumCubePowers).toEqual(86036)
   })
