ba1dcdf6f595ff19c4b729c81ac31e17cd8ba79d
Alexander Fischer <alexander.fischer@up2parts.com>
2023-12-18 08:34:12 +0100

    test: fix test wording

--- tests/day4/day4.test.ts
+++ tests/day4/day4.test.ts
@@ -10,7 +10,7 @@ const puzzleData = fs
   .readFileSync(path.resolve(__dirname, 'puzzleData.txt'), 'utf8')
   .split('\n')
 
-describe('day 3', () => {
+describe('day 4', () => {
   describe('part 1', () => {
     it('sample data', () => {
       expect(executePart1(sampleData)).toEqual(13)
