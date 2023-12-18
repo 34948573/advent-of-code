800c96eb6c8673781b669fa136529a513a76c472
Alexander Fischer <alexander.fischer@up2parts.com>
2023-12-18 08:33:42 +0100

    add files for day 4

---
+++ tests/day4/day4.test.ts
@@ -0,0 +1,23 @@
+import fs from 'fs'
+import path from 'path'
+import { execute as executePart1 } from '../../src/day4/part1'
+
+const sampleData = fs
+  .readFileSync(path.resolve(__dirname, 'sampleData.txt'), 'utf8')
+  .split('\n')
+
+const puzzleData = fs
+  .readFileSync(path.resolve(__dirname, 'puzzleData.txt'), 'utf8')
+  .split('\n')
+
+describe('day 3', () => {
+  describe('part 1', () => {
+    it('sample data', () => {
+      expect(executePart1(sampleData)).toEqual(13)
+    })
+
+    it('puzzle data', () => {
+      expect(executePart1(puzzleData)).toEqual(0)
+    })
+  })
+})
